3009dd2517eaff6a247fa753f207bbd2c2123523
nguyen_hoan <hoan@iastate.edu>
2017-01-23 12:32:33 -0600

    add visitor to generate sequence using new (simpler) rules. need testing

---
+++ src/parser/Sequencer.java
@@ -0,0 +1,682 @@
+package parser;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.eclipse.jdt.core.dom.*;
+
+public class Sequencer extends ASTVisitor {
+	private int numOfExpressions = 0, numOfResolvedExpressions = 0;
+	private StringBuilder fullTokens = new StringBuilder(), partialTokens = new StringBuilder();
+	
+	public Sequencer() {
+		super(false);
+	}
+
+	public StringBuilder getFullTokens() {
+		return fullTokens;
+	}
+
+	public StringBuilder getPartialTokens() {
+		return partialTokens;
+	}
+
+	public int getNumOfExpressions() {
+		return numOfExpressions;
+	}
+
+	public int getNumOfResolvedExpressions() {
+		return numOfResolvedExpressions;
+	}
+
+	private Type getType(VariableDeclarationFragment node) {
+		ASTNode p = node.getParent();
+		if (p instanceof VariableDeclarationExpression)
+			return ((VariableDeclarationExpression) p).getType();
+		if (p instanceof VariableDeclarationStatement)
+			return ((VariableDeclarationStatement) p).getType();
+		return null;
+	}
+
+	private static String getUnresolvedType(Type type) {
+		if (type.isArrayType()) {
+			ArrayType t = (ArrayType) type;
+			return getUnresolvedType(t.getElementType()) + getDimensions(t.getDimensions());
+		} else if (type.isIntersectionType()) {
+			IntersectionType it = (IntersectionType) type;
+			@SuppressWarnings("unchecked")
+			ArrayList<Type> types = new ArrayList<>(it.types());
+			String s = getUnresolvedType(types.get(0));
+			for (int i = 1; i < types.size(); i++)
+				s += " & " + getUnresolvedType(types.get(i));
+			return s;
+		} else if (type.isParameterizedType()) {
+			ParameterizedType t = (ParameterizedType) type;
+			return getUnresolvedType(t.getType());
+		} else if (type.isUnionType()) {
+			UnionType it = (UnionType) type;
+			@SuppressWarnings("unchecked")
+			ArrayList<Type> types = new ArrayList<>(it.types());
+			String s = getUnresolvedType(types.get(0));
+			for (int i = 1; i < types.size(); i++)
+				s += " | " + getUnresolvedType(types.get(i));
+			return s;
+		} else if (type.isNameQualifiedType()) {
+			NameQualifiedType qt = (NameQualifiedType) type;
+			return qt.getQualifier().getFullyQualifiedName() + "." + qt.getName().getIdentifier();
+		} else if (type.isPrimitiveType()) {
+			return type.toString();
+		} else if (type.isQualifiedType()) {
+			QualifiedType qt = (QualifiedType) type;
+			return getUnresolvedType(qt.getQualifier()) + "." + qt.getName().getIdentifier();
+		} else if (type.isSimpleType()) {
+			return type.toString();
+		} else if (type.isWildcardType()) {
+			WildcardType wt = (WildcardType) type;
+			String s = "?";
+			if (wt.getBound() != null) {
+				if (wt.isUpperBound())
+					s += "extends ";
+				else
+					s += "super ";
+				s += getUnresolvedType(wt.getBound());
+			}
+			return s;
+		}
+		
+		return null;
+	}
+
+	private static String getDimensions(int dimensions) {
+		String s = "";
+		for (int i = 0; i < dimensions; i++)
+			s += "[]";
+		return s;
+	}
+
+	private static String getResolvedType(Type type) {
+		if (type.isArrayType()) {
+			ArrayType t = (ArrayType) type;
+			return getResolvedType(t.getElementType()) + getDimensions(t.getDimensions());
+		} else if (type.isIntersectionType()) {
+			IntersectionType it = (IntersectionType) type;
+			@SuppressWarnings("unchecked")
+			ArrayList<Type> types = new ArrayList<>(it.types());
+			String s = getResolvedType(types.get(0));
+			for (int i = 1; i < types.size(); i++)
+				s += " & " + getResolvedType(types.get(i));
+			return s;
+		} else if (type.isParameterizedType()) {
+			ParameterizedType t = (ParameterizedType) type;
+			return getResolvedType(t.getType());
+		} else if (type.isUnionType()) {
+			UnionType it = (UnionType) type;
+			@SuppressWarnings("unchecked")
+			ArrayList<Type> types = new ArrayList<>(it.types());
+			String s = getResolvedType(types.get(0));
+			for (int i = 1; i < types.size(); i++)
+				s += " | " + getResolvedType(types.get(i));
+			return s;
+		} else if (type.isNameQualifiedType()) {
+			return type.resolveBinding().getTypeDeclaration().getQualifiedName();
+		} else if (type.isPrimitiveType()) {
+			return type.toString();
+		} else if (type.isQualifiedType()) {
+			return type.resolveBinding().getTypeDeclaration().getQualifiedName();
+		} else if (type.isSimpleType()) {
+			return type.resolveBinding().getTypeDeclaration().getQualifiedName();
+		} else if (type.isWildcardType()) {
+			WildcardType wt = (WildcardType) type;
+			String s = "?";
+			if (wt.getBound() != null) {
+				if (wt.isUpperBound())
+					s += "extends ";
+				else
+					s += "super ";
+				s += getResolvedType(wt.getBound());
+			}
+			return s;
+		}
+		
+		return null;
+	}
+
+	@Override
+	public void preVisit(ASTNode node) {
+		if (node instanceof Expression) {
+			numOfExpressions++;
+			Expression e = (Expression) node;
+			if (e.resolveTypeBinding() != null)
+				numOfResolvedExpressions++;
+		}
+	}
+
+	@Override
+	public boolean visit(ArrayAccess node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(ArrayCreation node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(ArrayInitializer node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(AssertStatement node) {
+		this.fullTokens.append(" assert ");
+		this.partialTokens.append(" assert ");
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(Assignment node) {
+		node.getLeftHandSide().accept(this);
+		this.fullTokens.append(" = ");
+		this.partialTokens.append(" = ");
+		node.getRightHandSide().accept(this);
+		return false;
+	}
+
+	@Override
+	public boolean visit(Block node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(BooleanLiteral node) {
+		this.fullTokens.append(" boolean ");
+		this.partialTokens.append(" boolean ");
+		return false;
+	}
+
+	@Override
+	public boolean visit(BreakStatement node) {
+		return false;
+	}
+
+	@Override
+	public boolean visit(CastExpression node) {
+		String utype = getUnresolvedType(node.getType()), rtype = getResolvedType(node.getType());
+		this.fullTokens.append(" " + rtype + " cast ");
+		this.partialTokens.append(" " + utype + " cast ");
+		node.getExpression().accept(this);
+		return false;
+	}
+
+	@Override
+	public boolean visit(CatchClause node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(CharacterLiteral node) {
+		this.fullTokens.append(" char ");
+		this.partialTokens.append(" char ");
+		return false;
+	}
+
+	@Override
+	public boolean visit(ClassInstanceCreation node) {
+		String utype = getUnresolvedType(node.getType()), rtype = getResolvedType(node.getType());
+		this.fullTokens.append(" " + rtype + " new ");
+		this.partialTokens.append(" " + utype + " new ");
+		for (Iterator it = node.arguments().iterator(); it.hasNext(); ) {
+			Expression e = (Expression) it.next();
+			e.accept(this);
+		}
+		if (node.getAnonymousClassDeclaration() != null)
+			node.getAnonymousClassDeclaration().accept(this);
+		return false;
+	}
+
+	@Override
+	public boolean visit(ConditionalExpression node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(ConstructorInvocation node) {
+		IMethodBinding b = node.resolveConstructorBinding();
+		if (b != null) {
+			ITypeBinding tb = b.getDeclaringClass().getTypeDeclaration();
+			this.partialTokens.append(" " + tb.getName() + " ");
+			this.fullTokens.append(" " + tb.getQualifiedName() + " ");
+		} else {
+			this.partialTokens.append(" this ");
+			this.fullTokens.append(" this ");
+		}
+		String name = ".<init>";
+		this.partialTokens.append(" " + name + " ");
+		if (b != null)
+			name = b.getDeclaringClass().getTypeDeclaration().getQualifiedName() + name;
+		this.fullTokens.append(" " + name + " ");
+		for (int i = 0; i < node.arguments().size(); i++)
+			((ASTNode) node.arguments().get(i)).accept(this);
+		return false;
+	}
+
+	@Override
+	public boolean visit(ContinueStatement node) {
+		return false;
+	}
+
+	@Override
+	public boolean visit(CreationReference node) {
+		return false;
+	}
+
+	@Override
+	public boolean visit(Dimension node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(DoStatement node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(EmptyStatement node) {
+		return false;
+	}
+
+	@Override
+	public boolean visit(EnhancedForStatement node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(EnumConstantDeclaration node) {
+		return false;
+	}
+
+	@Override
+	public boolean visit(EnumDeclaration node) {
+		return false;
+	}
+
+	@Override
+	public boolean visit(ExpressionMethodReference node) {
+		return false;
+	}
+
+	@Override
+	public boolean visit(ExpressionStatement node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(FieldAccess node) {
+		this.fullTokens.append(" ");
+		this.partialTokens.append(" ");
+		node.getExpression().accept(this);
+		String name = "." + node.getName().getIdentifier();
+		this.partialTokens.append(" " + name + " ");
+		IVariableBinding b = node.resolveFieldBinding();
+		if (b != null)
+			name = b.getDeclaringClass().getTypeDeclaration().getQualifiedName() + name;
+		this.fullTokens.append(" " + name + " ");
+		return false;
+	}
+
+	@Override
+	public boolean visit(FieldDeclaration node) {
+		return false;
+	}
+
+	@Override
+	public boolean visit(ForStatement node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(IfStatement node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(ImportDeclaration node) {
+		return false;
+	}
+
+	@Override
+	public boolean visit(InfixExpression node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(Initializer node) {
+		return false;
+	}
+
+	@Override
+	public boolean visit(InstanceofExpression node) {
+		this.fullTokens.append(" ");
+		this.partialTokens.append(" ");
+		node.getLeftOperand().accept(this);
+		this.fullTokens.append(" instanceof ");
+		this.partialTokens.append(" instanceof ");
+		String rtype = getResolvedType(node.getRightOperand()), utype = getUnresolvedType(node.getRightOperand());
+		this.fullTokens.append(rtype + " ");
+		this.partialTokens.append(utype + " ");
+		return false;
+	}
+
+	@Override
+	public boolean visit(LabeledStatement node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(LambdaExpression node) {
+		return false;
+	}
+
+	@Override
+	public boolean visit(MethodDeclaration node) {
+		node.getBody().accept(this);
+		return false;
+	}
+
+	@Override
+	public boolean visit(MethodInvocation node) {
+		this.fullTokens.append(" ");
+		this.partialTokens.append(" ");
+		IMethodBinding b = node.resolveMethodBinding();
+		if (node.getExpression() != null) {
+			node.getExpression().accept(this);
+		} else {
+			if (b != null) {
+				ITypeBinding tb = b.getDeclaringClass().getTypeDeclaration();
+				this.partialTokens.append(" " + tb.getName() + " ");
+				this.fullTokens.append(" " + tb.getQualifiedName() + " ");
+			} else {
+				this.partialTokens.append(" this ");
+				this.fullTokens.append(" this ");
+			}
+		}
+		String name = "."+ node.getName().getIdentifier();
+		this.partialTokens.append(" " + name + " ");
+		if (b != null)
+			name = b.getDeclaringClass().getTypeDeclaration().getQualifiedName() + name;
+		this.fullTokens.append(" " + name + " ");
+		for (int i = 0; i < node.arguments().size(); i++)
+			((ASTNode) node.arguments().get(i)).accept(this);
+		return false;
+	}
+
+	@Override
+	public boolean visit(Modifier node) {
+		return false;
+	}
+
+	@Override
+	public boolean visit(NormalAnnotation node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(NullLiteral node) {
+		this.fullTokens.append(" null ");
+		this.partialTokens.append(" null ");
+		return false;
+	}
+
+	@Override
+	public boolean visit(NumberLiteral node) {
+		this.fullTokens.append(" number ");
+		this.partialTokens.append(" number ");
+		return false;
+	}
+
+	@Override
+	public boolean visit(PackageDeclaration node) {
+		return false;
+	}
+
+	@Override
+	public boolean visit(ParenthesizedExpression node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(PostfixExpression node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(PrefixExpression node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(QualifiedName node) {
+		node.getQualifier().accept(this);
+		String name = "." + node.getName().getIdentifier();
+		this.partialTokens.append(" " + name + " ");
+		IBinding b = node.resolveBinding();
+		if (b != null) {
+			if (b instanceof IVariableBinding) {
+				IVariableBinding vb = (IVariableBinding) b;
+				name = vb.getDeclaringClass().getTypeDeclaration().getQualifiedName() + name;
+			}
+		}
+		this.fullTokens.append(" " + name + " ");
+		return false;
+	}
+
+	@Override
+	public boolean visit(ReturnStatement node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(SimpleName node) {
+		IBinding b = node.resolveBinding();
+		if (b != null) {
+			if (b instanceof IVariableBinding) {
+				IVariableBinding vb = (IVariableBinding) b;
+				this.fullTokens.append(" " + vb.getType().getTypeDeclaration().getQualifiedName() + " ");
+				this.partialTokens.append(" " + vb.getType().getTypeDeclaration().getName() + " ");
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean visit(SingleMemberAnnotation node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(SingleVariableDeclaration node) {
+		String utype = getUnresolvedType(node.getType()), rtype = getResolvedType(node.getType());
+		this.partialTokens.append(" " + utype + " ");
+		this.fullTokens.append(" " + rtype + " ");
+		if (node.getInitializer() != null)
+			node.getInitializer().accept(this);
+		return false;
+	}
+
+	@Override
+	public boolean visit(StringLiteral node) {
+		this.fullTokens.append(" String ");
+		this.partialTokens.append(" String ");
+		return false;
+	}
+
+	@Override
+	public boolean visit(SuperConstructorInvocation node) {
+		IMethodBinding b = node.resolveConstructorBinding();
+		if (b != null) {
+			ITypeBinding tb = b.getDeclaringClass().getTypeDeclaration();
+			this.partialTokens.append(" " + tb.getName() + " ");
+			this.fullTokens.append(" " + tb.getQualifiedName() + " ");
+		} else {
+			this.partialTokens.append(" super ");
+			this.fullTokens.append(" super ");
+		}
+		String name = ".<init>";
+		this.fullTokens.append(" " + name + " ");
+		if (b != null)
+			name = b.getDeclaringClass().getTypeDeclaration().getQualifiedName() + name;
+		this.partialTokens.append(" " + name + " ");
+		for (int i = 0; i < node.arguments().size(); i++)
+			((ASTNode) node.arguments().get(i)).accept(this);
+		return false;
+	}
+
+	@Override
+	public boolean visit(SuperFieldAccess node) {
+		IVariableBinding b = node.resolveFieldBinding();
+		if (b != null) {
+			ITypeBinding tb = b.getDeclaringClass().getTypeDeclaration();
+			this.partialTokens.append(" " + tb.getName() + " ");
+			this.fullTokens.append(" " + tb.getQualifiedName() + " ");
+		} else {
+			this.partialTokens.append(" super ");
+			this.fullTokens.append(" super ");
+		}
+		String name = "." + node.getName().getIdentifier();
+		this.partialTokens.append(" " + name + " ");
+		if (b != null)
+			name = b.getDeclaringClass().getTypeDeclaration().getQualifiedName() + name;
+		this.fullTokens.append(" " + name + " ");
+		return false;
+	}
+
+	@Override
+	public boolean visit(SuperMethodInvocation node) {
+		IMethodBinding b = node.resolveMethodBinding();
+		if (b != null) {
+			ITypeBinding tb = b.getDeclaringClass().getTypeDeclaration();
+			this.partialTokens.append(" " + tb.getName() + " ");
+			this.fullTokens.append(" " + tb.getQualifiedName() + " ");
+		} else {
+			this.partialTokens.append(" super ");
+			this.fullTokens.append(" super ");
+		}
+		String name = "."+ node.getName().getIdentifier();
+		this.partialTokens.append(" " + name + " ");
+		if (b != null)
+			name = b.getDeclaringClass().getTypeDeclaration().getQualifiedName() + name;
+		this.fullTokens.append(" " + name + " ");
+		for (int i = 0; i < node.arguments().size(); i++)
+			((ASTNode) node.arguments().get(i)).accept(this);
+		return false;
+	}
+
+	@Override
+	public boolean visit(SuperMethodReference node) {
+		return false;
+	}
+
+	@Override
+	public boolean visit(SwitchCase node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(SwitchStatement node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(SynchronizedStatement node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(ThisExpression node) {
+		ITypeBinding b = node.resolveTypeBinding();
+		if (b != null) {
+			b = b.getTypeDeclaration();
+			this.partialTokens.append(" " + b.getName() + " ");
+			this.fullTokens.append(" " + b.getQualifiedName() + " ");
+		} else {
+			this.partialTokens.append(" this ");
+			this.fullTokens.append(" this ");
+		}
+		return false;
+	}
+
+	@Override
+	public boolean visit(ThrowStatement node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(TryStatement node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(TypeDeclaration node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(TypeDeclarationStatement node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(TypeLiteral node) {
+		String utype = getUnresolvedType(node.getType()), rtype = getResolvedType(node.getType());
+		this.fullTokens.append(" " + rtype + ".class ");
+		this.partialTokens.append(" " + utype + ".class ");
+		return false;
+	}
+
+	@Override
+	public boolean visit(TypeMethodReference node) {
+		return super.visit(node);
+	}
+
+	@Override
+	public boolean visit(TypeParameter node) {
+		return super.visit(node);
+	}
+	
+	@Override
+	public boolean visit(VariableDeclarationExpression node) {
+		String utype = getUnresolvedType(node.getType()), rtype = getResolvedType(node.getType());
+		this.partialTokens.append(" " + utype + " ");
+		this.fullTokens.append(" " + rtype + " ");
+		for (int i = 0; i < node.fragments().size(); i++)
+			((ASTNode) node.fragments().get(i)).accept(this);
+		return false;
+	}
+
+	@Override
+	public boolean visit(VariableDeclarationStatement node) {
+		String utype = getUnresolvedType(node.getType()), rtype = getResolvedType(node.getType());
+		this.partialTokens.append(" " + utype + " ");
+		this.fullTokens.append(" " + rtype + " ");
+		for (int i = 0; i < node.fragments().size(); i++)
+			((ASTNode) node.fragments().get(i)).accept(this);
+		return false;
+	}
+
+	@Override
+	public boolean visit(VariableDeclarationFragment node) {
+		Type type = getType(node);
+		String utype = getUnresolvedType(type), rtype = getResolvedType(type);
+		this.partialTokens.append(" " + utype + " ");
+		this.fullTokens.append(" " + rtype + " ");
+		if (node.getInitializer() != null)
+			node.getInitializer().accept(this);
+		return false;
+	}
+
+	@Override
+	public boolean visit(WhileStatement node) {
+		return super.visit(node);
+	}
+
+}
