f420cf3d8c710f7c5361672b909fa1cdf819c711
nguyen_hoan <hoan@iastate.edu>
2017-01-26 00:24:00 -0600

    use simple method names

--- src/parser/SequenceGenerator.java
+++ src/parser/SequenceGenerator.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import org.eclipse.jdt.core.dom.*;
 
 public class SequenceGenerator extends ASTVisitor {
+	private static final boolean USE_SIMPLE_METHOD_NAME = true;
 	private String className, superClassName;
 	private int numOfExpressions = 0, numOfResolvedExpressions = 0;
 	private StringBuilder fullTokens = new StringBuilder(), partialTokens = new StringBuilder();
@@ -521,7 +522,14 @@ public class SequenceGenerator extends ASTVisitor {
 			}
 			String name = "."+ node.getName().getIdentifier() + "()";
 			this.partialTokens.append(" " + name + " ");
-			if (tb != null)
+			if (!USE_SIMPLE_METHOD_NAME && tb != null 
+					&& !name.equals(".toString()") 
+					&& !name.equals(".equals()") 
+					&& !name.equals(".clone()") 
+					&& !name.equals(".getClass()")
+					&& !name.equals(".hashCode()")
+					&& !name.equals(".valueOf()")
+					)
 				name = tb.getQualifiedName() + name;
 			this.fullTokens.append(" " + name + " ");
 		}
@@ -737,7 +745,14 @@ public class SequenceGenerator extends ASTVisitor {
 		}
 		String name = "."+ node.getName().getIdentifier() + "()";
 		this.partialTokens.append(" " + name + " ");
-		if (tb != null)
+		if (!USE_SIMPLE_METHOD_NAME && tb != null 
+				&& !name.equals(".toString()") 
+				&& !name.equals(".equals()") 
+				&& !name.equals(".clone()") 
+				&& !name.equals(".getClass()")
+				&& !name.equals(".hashCode()")
+				&& !name.equals(".valueOf()")
+				)
 			name = tb.getQualifiedName() + name;
 		this.fullTokens.append(" " + name + " ");
 		for (int i = 0; i < node.arguments().size(); i++)
