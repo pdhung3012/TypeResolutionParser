6f41f8a5cd7eaeb8aeb0d6a3dfa1ff734be3486b
hungphd <hungphd@iastate.edu>
2017-02-02 19:37:27 -0600

    Evaluate precision and recall with windows translation

--- src/parser/EvaluateInOutPrecisionRecall.java
+++ src/parser/EvaluateInOutPrecisionRecall.java
@@ -6,14 +6,14 @@ import utils.FileUtil;
 
 public class EvaluateInOutPrecisionRecall {
 
-	static String fop_input="C:\\Users\\pdhung\\Desktop\\hungData\\research\\ImportantProjects\\SpecMiningProject\\TypeResolutionTranslation\\output20170125\\";
+	static String fop_input="C:\\Users\\pdhung\\Desktop\\hungData\\research\\ImportantProjects\\SpecMiningProject\\TypeResolutionTranslation\\output100K\\reordered\\";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String fn_trainSource="train.s";
 		String fn_trainTarget="train.t";
 		String fn_testSource="test.s";
 		String fn_testTarget="test.t";
-		String fn_testTranslation="backup_InOutVocabTest.trans";
+		String fn_testTranslation="correct.trans";
 		String fn_result="result.txt";
 		String fn_log_incorrect="log_incorrect.txt";
 		
@@ -22,11 +22,22 @@ public class EvaluateInOutPrecisionRecall {
 		String[] arrTestSource=FileUtil.getFileContent(fop_input+fn_testSource).trim().split("\n");
 		String[] arrTestTarget=FileUtil.getFileContent(fop_input+fn_testTarget).trim().split("\n");
 		String[] arrTestTranslation=FileUtil.getFileContent(fop_input+fn_testTranslation).trim().split("\n");
+		String[] arrEvaluatedTypes=FileUtil.getFileContent(fop_input+"evaluatedResults.txt").trim().split("\n");
 		
 		HashSet<String> setVocabTrainSource=new HashSet<String>();
 		HashSet<String> setVocabTrainTarget=new HashSet<String>();
 		HashSet<String> setVocabMap=new HashSet<String>();
 		
+		
+		
+		HashSet<Integer> lstNotReorderedLine=new HashSet<Integer>();
+		
+		for(int i=0;i<arrEvaluatedTypes.length;i++){
+			String[] arrItems=arrEvaluatedTypes[i].split("\t");
+			if(arrItems[1].equals("true")){
+				lstNotReorderedLine.add(i+1);
+			}
+		}
 		
 		
 		for(int i=0;i<arrTrainSource.length;i++){
@@ -52,13 +63,18 @@ public class EvaluateInOutPrecisionRecall {
 		FileUtil.writeToFile(fop_input+fn_log_incorrect, "");
 		
 		for(int i=0;i<arrTestSource.length;i++){
+			if(!lstNotReorderedLine.contains(i+1)){
+				continue;
+			}
 			String[] itemSource=arrTestSource[i].trim().split("\\s+");
 			String[] itemTarget=arrTestTarget[i].trim().split("\\s+");
 			String[] itemTrans=arrTestTranslation[i].trim().split("\\s+");
 			String strIncorrectLog="";
 			
 			int numCSourceLine=0,numCTargetLine=0,numIncorrect=0,numCorrect=0;
+			System.out.println("Line "+i);
 			for(int j=0;j<itemSource.length;j++){
+				
 				if(itemTarget[j].contains(".")){
 					if(!setVocabTrainSource.contains(itemSource[j])){
 						numCSourceLine++;
