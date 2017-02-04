b6ae17128bf8d94df68c94d00ebe6f4d37967ba7
nguyen_hoan <hoan@iastate.edu>
2017-02-04 17:57:25 -0600

    use string pattern matching instead of parsing files into ASTs

--- src/data/GetMostUsingProjects.java
+++ src/data/GetMostUsingProjects.java
@@ -3,11 +3,11 @@ package data;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 import org.eclipse.jdt.core.JavaCore;
@@ -29,12 +29,18 @@ public class GetMostUsingProjects {
 		File dir = new File("G:/github/repos-5stars-50commits");
 		for (File udir : dir.listFiles()) {
 			for (File pdir : udir.listFiles()) {
-				HashMap<String, Integer> libCount = new HashMap<>();
-				for (String lib : libs)
-					libCount.put(lib, 0);
-				getMostUsingProjects(pdir, libCount);
-				for (String lib : libCount.keySet())
-					libps.get(lib).println(udir.getName() + "/" + pdir.getName() + "," + libCount.get(lib));
+				HashMap<String, Integer> libFileCount = new HashMap<>(), libImportCount = new HashMap<>(), libPackageCount = new HashMap<>();
+				for (String lib : libs) {
+					libFileCount.put(lib, 0);
+					libImportCount.put(lib, 0);
+					libPackageCount.put(lib, 0);
+				}
+				getMostUsingProjects(pdir, libFileCount, libImportCount, libPackageCount);
+				for (String lib : libFileCount.keySet())
+					libps.get(lib).println(udir.getName() + "/" + pdir.getName() 
+					+ "," + libFileCount.get(lib)
+					+ "," + libImportCount.get(lib)
+					+ "," + libPackageCount.get(lib));
 			}
 		}
 		for (Map.Entry<String, PrintStream> e : libps.entrySet()) {
@@ -43,43 +49,71 @@ public class GetMostUsingProjects {
 		}
 	}
 
-	private static void getMostUsingProjects(File file, HashMap<String, Integer> counts) {
+	private static void getMostUsingProjects(File file, HashMap<String, Integer> libFileCount, HashMap<String, Integer> libImportCount, HashMap<String, Integer> libPackageCount) {
 		if (file.isDirectory()) {
 			if (file.getName().equals(".git"))
 				return;
 			for (File sub : file.listFiles())
-				getMostUsingProjects(sub, counts);
+				getMostUsingProjects(sub, libFileCount, libImportCount, libPackageCount);
 		} else if (file.getName().endsWith(".java")) {
-			Map options = JavaCore.getOptions();
-			options.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_8);
-			options.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, JavaCore.VERSION_1_8);
-			options.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_1_8);
-			ASTParser parser = ASTParser.newParser(AST.JLS8);
-			parser.setCompilerOptions(options);
-			parser.setSource(FileUtil.getFileContent(file.getAbsolutePath()).toCharArray());
-			try {
-				CompilationUnit ast = (CompilationUnit) parser.createAST(null);
-				if (ast.getPackage() != null && !ast.types().isEmpty() && ast.types().get(0) instanceof TypeDeclaration) {
-					HashSet<String> usedLibs = new HashSet<>();
-					String pn = ast.getPackage().getName().getFullyQualifiedName();
-					check(pn, counts.keySet(), usedLibs);
-					for (int i = 0; i < ast.imports().size(); i++) {
-						ImportDeclaration id = (ImportDeclaration) ast.imports().get(i);
-						pn = id.getName().getFullyQualifiedName();
-						check(pn, counts.keySet(), usedLibs);
-					}
-					for (String lib : usedLibs)
-						counts.put(lib, counts.get(lib) + 1);
-				}
-			} catch (Throwable t) {}
+			String content = FileUtil.getFileContent(file.getAbsolutePath());
+			HashSet<String> usedLibs = new HashSet<>();
+			check(content, libFileCount.keySet(), usedLibs, libImportCount, libPackageCount);
+			for (String lib : usedLibs)
+				libFileCount.put(lib, libFileCount.get(lib) + 1);
+//			Map options = JavaCore.getOptions();
+//			options.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_8);
+//			options.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, JavaCore.VERSION_1_8);
+//			options.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_1_8);
+//			ASTParser parser = ASTParser.newParser(AST.JLS8);
+//			parser.setCompilerOptions(options);
+//			parser.setSource(content.toCharArray());
+//			try {
+//				CompilationUnit ast = (CompilationUnit) parser.createAST(null);
+//				if (ast.getPackage() != null && !ast.types().isEmpty() && ast.types().get(0) instanceof TypeDeclaration) {
+//					String pn = ast.getPackage().getName().getFullyQualifiedName();
+//					check(pn, libFileCount.keySet(), usedLibs, libPackageCount);
+//					for (int i = 0; i < ast.imports().size(); i++) {
+//						ImportDeclaration id = (ImportDeclaration) ast.imports().get(i);
+//						pn = id.getName().getFullyQualifiedName();
+//						check(pn, libFileCount.keySet(), usedLibs, libImportCount);
+//					}
+//					for (String lib : usedLibs)
+//						libFileCount.put(lib, libFileCount.get(lib) + 1);
+//				}
+//			} catch (Throwable t) {}
 		}
 		return;
 	}
 
-	private static void check(String pn, Set<String> libs, HashSet<String> usedLibs) {
+	private static void check(String content, Set<String> libs, HashSet<String> usedLibs, HashMap<String, Integer> libImportCount, HashMap<String, Integer> libPackageCount) {
+		Scanner sc = new Scanner(content);
+		boolean seen = false, hasPackage = false;
+		while (sc.hasNextLine()) {
+			String line = sc.nextLine().trim();
+			if (line.startsWith("package ") && line.endsWith(";")) {
+				check(line.substring("package ".length()), libs, usedLibs, libPackageCount);
+				hasPackage = true;
+			}
+			if (hasPackage) {
+				if (line.startsWith("import ") && line.endsWith(";")) {
+					seen = true;
+					check(line.substring("import ".length()), libs, usedLibs, libImportCount);
+				}
+				else if (!line.isEmpty()) {
+					if (seen)
+						break;
+				}
+			}
+		}
+		sc.close();
+	}
+
+	private static void check(String pn, Set<String> libs, HashSet<String> usedLibs, HashMap<String, Integer> libCount) {
 		for (String lib : libs) {
 			if (pn.startsWith(lib)) {
 				usedLibs.add(lib);
+				libCount.put(lib, libCount.get(lib) + 1);
 				break;
 			}
 		}
